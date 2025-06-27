6b7baf83ca93985ea438095d34782462ff8a6d0f
Saddiq Rupani <saddiqrupani456@gmail.com>
2025-06-27 19:10:53 -0400

    Created Theme Toggle and connected to local storage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
     return (
-        <div className="min-h-screen ">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
             { /* Theme toggle button */ }
 
+            <ThemeToggle />
             { /* Background Effects */ }
 
             { /* Navbar */ }
