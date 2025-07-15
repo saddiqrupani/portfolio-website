a84e58b651149cfc516348b28918c375ea536742
Saddiq Rupani <saddiqrupani456@gmail.com>
2025-07-15 17:40:08 -0400

    Finished Theme Toggle and Background Effects, Work on Navbar next

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-
             { /* Theme toggle button */ }
-
             <ThemeToggle />
             { /* Background Effects */ }
-
+            <StarBackground />
             { /* Navbar */ }
 
             { /* Main Content */ }
